cfffd084267746889f0690a8da5a94ea8bf185f7
hehongchen <hehongcheng@qq.com>
2022-06-29 13:55:40 +0800

    客户功能

--- src/views/CustomerManage/customer/customerDetail/index.vue
+++ src/views/CustomerManage/customer/customerDetail/index.vue
@@ -126,80 +126,138 @@
           </div>
         </el-col>
       </el-row>
-      <el-table
-        :data="tableData"
-        style="width: 100%"
-        :header-row-class-name="setTableHeaderClass"
-        height="250px"
-      >
-        <el-table-column label="#" type="index" align="center" width="60">
-        </el-table-column>
-        <el-table-column
-          prop="addr"
-          label="收货地址"
-          width=""
-        >
-        </el-table-column>
-        <el-table-column prop="addrType" label="地址类型" width="100" align="center">
-        </el-table-column>
-        <el-table-column prop="useTime" label="使用次数" width="100" align="center">
-        </el-table-column>
-        <el-table-column prop="lastTime" label="最近使用时间"  width="160" align="center">
-        </el-table-column>
-        <el-table-column label="操作" align="center"  width="180">
-          <template>
-            <el-button type="text">失效</el-button>
-            <span style="margin: 0 5px">|</span>
-            <el-button type="text">修改</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <el-tabs>
+        <el-tab-pane label="地址信息">
+          <el-table
+            :data="tableData"
+            style="width: 100%"
+            :header-row-class-name="setTableHeaderClass"
+            height="210px"
+          >
+            <el-table-column label="#" type="index" align="center" width="60">
+            </el-table-column>
+            <el-table-column prop="addr" label="收货地址" width="">
+              <template slot-scope="{ row }">
+                <div class="addr_box">
+                  <div class="addr_left_box">
+                    <common-avatar :text="row.name"></common-avatar>
+                  </div>
+                  <div class="addr_right_box">
+                    <div class="addr_right">
+                      <span>{{row.name}}</span>
+                      <span>{{row.phone}}</span>
+                    </div>
+                    <div class="addr_right">
+                      <el-tag type="success" size="mini" v-if="row.isDefault === 1">默认地址</el-tag>
+                      <span>{{row.addr}}</span>
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="addrType"
+              label="地址类型"
+              width="100"
+              align="center"
+            >
+            </el-table-column>
+            <el-table-column
+              prop="useTime"
+              label="使用次数"
+              width="100"
+              align="center"
+            >
+            </el-table-column>
+            <el-table-column
+              prop="lastTime"
+              label="最近使用时间"
+              width="160"
+              align="center"
+            >
+            </el-table-column>
+            <el-table-column label="操作" width="200">
+              <template slot-scope="{ row }">
+                <el-button type="text">失效</el-button>
+                <span style="margin: 0 5px">|</span>
+                <el-button type="text">修改</el-button>
+                <span style="margin: 0 5px" v-if="row.isDefault === 2">|</span>
+                <el-button type="text" v-if="row.isDefault === 2">设为默认地址</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="沟通历史">沟通历史</el-tab-pane>
+        <el-tab-pane label="短信">短信</el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import commonAvatar from '@/components/common/commonAvatar'
 export default {
+  components: {
+    commonAvatar
+  },
   data() {
     return {
       circleUrl: require('@/assets/images/1.png'),
       tableData: [
         {
+          name: '雷无桀',
+          phone: '15689360191',
           addr: '山东省枣庄市滕州市学院中路50号山东墨辰信息科技有限公司',
           addrType: '公司',
           useTime: 12,
-          lastTime: '2022-06-26'
+          lastTime: '2022-06-26',
+          isDefault: 1
         },
         {
+          name: '司空长风',
+          phone: '15689360192',
           addr: '山东省枣庄市滕州市学院中路50号昊洋大厦',
           addrType: '家',
           useTime: 12,
-          lastTime: '2022-06-26'
+          lastTime: '2022-06-26',
+          isDefault: 2
         },
         {
+          name: '唐三',
+          phone: '15689360193',
           addr: '山东省枣庄市滕州市学院中路50号山东墨辰信息科技有限公司',
           addrType: '公司',
           useTime: 12,
-          lastTime: '2022-06-26'
+          lastTime: '2022-06-26',
+          isDefault: 2
         },
         {
+          name: '无心',
+          phone: '15689360194',
           addr: '山东省枣庄市滕州市学院中路50号昊洋大厦',
           addrType: '家',
           useTime: 12,
-          lastTime: '2022-06-26'
+          lastTime: '2022-06-26',
+          isDefault: 2
         },
         {
+          name: '张无忌',
+          phone: '15689360195',
           addr: '山东省枣庄市滕州市学院中路50号山东墨辰信息科技有限公司',
           addrType: '公司',
           useTime: 12,
-          lastTime: '2022-06-26'
+          lastTime: '2022-06-26',
+          isDefault: 2
         },
         {
+          name: '赵敏',
+          phone: '15689360196',
           addr: '山东省枣庄市滕州市学院中路50号昊洋大厦',
           addrType: '家',
           useTime: 12,
-          lastTime: '2022-06-26'
+          lastTime: '2022-06-26',
+          isDefault: 2
         }
       ]
     };
@@ -226,7 +284,7 @@ export default {
 <style scoped>
 @import url('../../../../assets/css/customer.css');
 .el-table >>> .table_header th {
-  background: rgb(29,116,255);
+  background: rgb(29, 116, 255);
   color: #fff;
 }
 </style>
