c41add7f2766ca8dba85252c36ef0594c1caf24e
hehongchen <hehongcheng@qq.com>
2022-07-12 10:31:43 +0800

    会员分组

--- src/views/memberManage/memberCube/index.vue
+++ src/views/memberManage/memberCube/index.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>会员魔方</div>
+  <div class="main_box">
+    <el-row :gutter="10">
+      <el-col :span="6">
+        <el-button
+          type="success"
+          icon="el-icon-circle-plus-outline"
+          plain
+          @click="dialogVisible = true"
+          >新建魔方</el-button
+        >
+      </el-col>
+    </el-row>
+    <div class="member_cube_box">
+        <div class="member_cube_item">
+            <el-card shadow="hover">
+                <div slot="header">会员魔方1</div>
+                <div></div>
+            </el-card>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {}
-    }
-}
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style>
-
+@import url('../../../assets/css/member.css');
 </style>
