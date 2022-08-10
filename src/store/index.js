da6b12912a931e09f412da4a623729ac2dd9e0ab
hehongchen <hehongcheng@qq.com>
2022-08-10 09:28:20 +0800

    vuex持久化

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,19 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import tabs from '@/store/modules/tabs'
 import common from '@/store/modules/common'
+import createPersistedState from "vuex-persistedstate"
 
 Vue.use(Vuex)
+
+// const dataState  = createPersistedState({
+//   key: 'tabsList',
+//   paths: ['tabsList'],
+// })
 
 export default new Vuex.Store({
   modules: {
     tabs,
     common
-  }
+  },
+  plugins: [createPersistedState({ storage: window.sessionStorage })]
 })
